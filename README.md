e5521d6f101ec9b6539c1bf0e8c80d7f60248606
Casey Kolderup <casey@kolderup.org>
2011-05-21 14:18:15 -0400

    Big Fan is functional and dressed up a little bit

---
+++ README.md
@@ -0,0 +1,13 @@
+# The Shoebox
+
+A collection of small .rb files for use with [Shoes][shoesrb].
+The repository was started for the [Two Shoes Weekend][tsw], but hopefully there will be more to come.
+
+## Big Fan (bigfan.rb)
+
+Big Fan displays the most recent tweets that were favorited by @ckolderup on Twitter one at a time.
+You can navigate between the tweets using the up/down arrow keys, page up, page down, home and end.
+
+
+[shoesrb]: http://shoesrb.com
+[tsw]: http://ckolderup.tumblr.com/post/5674571526/two-shoes-weekend
--- bigfan.rb
+++ bigfan.rb
@@ -1,26 +1,101 @@
 require 'net/http'
 require 'json'
 
-#TODO: only show one tweet at a time
-#TODO: add keyboard shortcuts for navigating between tweets
-#TODO: style the single-tweet view a bunch
-#TODO: add userpic
-#TODO: add profile background image?
-#TODO: add a way to get a link to the tweet/open in browser
- 
-Shoes.app :title => "Big Fan" do
+Shoes.app :height => 300, :width => 750, :title => "Big Fan", do
+  background cornflowerblue
+  @cur = 0
+  @timestamp 
+  @progress
+  
+  stack :height => 250, :top => 25, :width => 700, :left => 25 do
+    background white
+    @display = stack :height => 225 do
+      para "Loading..."
+    end
+    flow do
+      @timestamp = stack :width => 600 do
+        inscription ""
+      end
+      @progress = stack :width => 50, :right => 5 do
+        inscription ""
+      end
+    end
+  end
+
   uri = URI.parse("http://www.twitter.com/favorites/ckolderup.json")
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
-  @tweets = JSON.parse response.body
 
-  stack do
-    @tweets.each do |tweet|
-      flow do
-        para "@#{tweet["user"]["screen_name"]}"
-        para tweet["text"]
+  if (response.code != "200") then
+    @display.clear
+    @display.append { para "HTTP Return #{response.code}" }
+    
+  else
+    #parse tweets
+    @tweets = JSON.parse response.body
+    
+    #preload images
+    stack :height => 20 do
+      hide
+      @tweets.each do |x|
+        image x["user"]["profile_image_url"]
       end
     end
+    
+    #draw 
+    draw
+  end
+
+  keypress do |k|
+    #changes
+    @cur = @cur + 1 if (k == :down || k == :page_down || k == :space)
+    @cur = @cur - 1 if (k == :up || k == :page_up || k == :shift_space)
+    @cur = 0 if (k == :home)
+    @cur = @tweets.size - 1 if (k == :end)
+
+    #boundary checking
+    @cur = 0 if @cur < 0
+    @cur = @tweets.size - 1 if @cur >= @tweets.size
+
+    #repaint
+    draw
+  end
+end
+
+def draw
+  tweet = @tweets[@cur]
+  username = tweet["user"]["screen_name"]
+  profile_link = "http://www.twitter.com/#{username}"
+  name = tweet["user"]["name"]
+  text = tweet["text"]
+  ts = tweet["created_at"]
+  userpic = tweet["user"]["profile_image_url"]
+  permalink = "http://www.twitter.com/#{username}/status/#{tweet["id"]}"
+  
+  @display.clear do
+
+    flow :left => 15 do
+      stack :width => 50, :left => 10, :top => 10 do
+        image userpic
+      end
+      stack :width => 300, :top => 10 do
+        caption link("@#{username}", 
+          :click => profile_link),
+          :margin_bottom => 2
+        para name
+      end
+    end
+    flow :left => 10 do
+      tagline text
+    end
+  end
+  
+  @timestamp.clear do
+    inscription link(ts, :click => permalink)
+  end
+
+  @progress.clear do
+    inscription "#{@cur+1}/#{@tweets.size}"
   end
 end
